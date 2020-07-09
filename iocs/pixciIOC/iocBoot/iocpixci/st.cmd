3eb7b62758c77ab0728cfc2486bfccf33f39f5fe
Subindev D <subindev.devadasan@stfc.ac.uk>
2020-07-09 15:10:22 +0100

    code cleaned and add more comments

--- iocs/pixciIOC/iocBoot/iocpixci/st.cmd
+++ iocs/pixciIOC/iocBoot/iocpixci/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/windows-x64/pixci
 
-#- You may have to change pixci to something else
-#- everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -18,30 +15,24 @@ epicsEnvSet("PORT",   "PS1")
 # The queue size for all plugins
 epicsEnvSet("QSIZE",  "20")
 # The maximim image width; used for row profiles in the NDPluginStats plugin
-epicsEnvSet("XSIZE",  "1360")
+epicsEnvSet("XSIZE",  "600")
 # The maximim image height; used for column profiles in the NDPluginStats plugin
-epicsEnvSet("YSIZE",  "1024")
+epicsEnvSet("YSIZE",  "600")
 # The maximum number of time series points in the NDPluginStats plugin
-epicsEnvSet("NCHANS", "2048")
-# The maximum number of frames buffered in the NDPluginCircularBuff plugin
-epicsEnvSet("CBUFFS", "500")
+epicsEnvSet("NCHANS", "512")
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 
 #pixciConfig(portName, maxBuffers,maxMemory,priority,stackSize)
-
 pixciConfig("$(PORT)", 0,  0, 0, 0)
 dbLoadRecords("ADBase.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
-#dbLoadRecords("db/xxx.db","user=mii48756")
 
-asynSetTraceIOMask("$(PORT)",0,2)
-#asynSetTraceMask("$(PORT)",-1,0x9) 
+asynSetTraceIOMask("$(PORT)",0,0x2)
+asynSetTraceMask("$(PORT)",0,0x9) 
 #asynSetTraceMask("$(PORT)",0,ASYN_TRACE_ERROR+ASYN_TRACE_WARNING+ASYN_TRACE_FLOW)
-asynSetTraceMask("$(PORT)",0,ASYN_TRACE_ERROR+ASYN_TRACE_WARNING)
+#asynSetTraceMask("$(PORT)",0,ASYN_TRACE_ERROR+ASYN_TRACE_WARNING)
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=mii48756"
