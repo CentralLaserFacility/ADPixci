f1ab0043b45ffcdd83e634c493b06c47da7d5707
Subindev Devadasan <64056430+subindev-d@users.noreply.github.com>
2020-08-04 13:03:05 +0100

    code review changes

--- pixciApp/src/pixci.cpp
+++ pixciApp/src/pixci.cpp
@@ -5,7 +5,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <exception> 
 
 /* For windows */
 #if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
@@ -33,19 +32,22 @@ extern "C"{
 #include <epicsExit.h>
 #include <epicsExport.h>
 
-#define FORMAT "default" // Video format configuration name
-#define DRIVERPARMS "" //default , user '-QU 0' for not using interrupts
-#define SETUPFILE "" //Video format configuration file name
-#define UNIT 1
-/**
+#define FORMAT "default" // Video format configuration name.
+#define DRIVERPARMS "" // Default , user '-QU 0' for not using interrupts.
+#define SETUPFILE "" // Video format configuration file name.
+#define UNIT 1 // Unit to be selected for streaming, eb1 model only have 1 unit.
+#define NOERROR 0 // Errors are defined as integers below zero.
+
+/*
  * @brief C Function prototypes to tie in with EPICS
  * run acquire task 
  * @param drvPvt 
  */
 static void acquireTaskC(void *drvPvt);
-HANDLE  hEvent;
+/* Event handler for acquire task */
+HANDLE  g_hEvent; 
 
-/**
+/*
  * @brief Configuration command for pixci driver; creates a new pixci object.
  * @param See the pixci.h
  */
@@ -56,7 +58,7 @@ extern "C" int pixciConfig(const char *portName,
     return(asynSuccess);
 }
 
-/**
+/*
  * @brief Default constructor to create a new Pixci::Pixci object
  */
 Pixci::Pixci(const char *portName,  int maxBuffers, size_t maxMemory, int priority, int stackSize)
@@ -72,7 +74,7 @@ Pixci::Pixci(const char *portName,  int maxBuffers, size_t maxMemory, int priori
          * pxd_mesgErrorCode(int code) will return description of the error occured
          */
         connectionStatusCode = pxd_PIXCIopen(DRIVERPARMS, FORMAT, SETUPFILE);
-        if(connectionStatusCode < 0){          
+        if(connectionStatusCode < NOERROR){          
             asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, 
                   "%s: Cannot OPEN camera: %s.", 
                   driverName,  pxd_mesgErrorCode(connectionStatusCode));
@@ -83,11 +85,11 @@ Pixci::Pixci(const char *portName,  int maxBuffers, size_t maxMemory, int priori
             driverName);
         }
 
-        /*any thread waiting upon the event will be notified whenever a field has beencaptured by pxd_goSnap, 
+        /* Any thread waiting upon the event will be notified whenever a field has been captured by pxd_goSnap, 
         pxd_goLive, pxd_goLivePair and pxd_goLiveSeq*/
-        hEvent = pxd_eventCapturedFieldCreate(UNIT);
+        g_hEvent = pxd_eventCapturedFieldCreate(UNIT);
         int status = asynSuccess;
-        /*Create the thread that does data acquisition */
+        /* Create the thread that does data acquisition */
         status = (epicsThreadCreate("acquireTask",
                               epicsThreadPriorityMedium,
                               epicsThreadGetStackSize(epicsThreadStackMedium),
@@ -99,13 +101,13 @@ Pixci::Pixci(const char *portName,  int maxBuffers, size_t maxMemory, int priori
 Pixci::~Pixci(){
 
     /* Closing connection to frame grabber */
-    int disconnectStatusCode = 0;
+    int disconnectStatusCode = NOERROR;
     /*pxd_PIXCIclose() disconnect the driver from the device. 
      * return 0 if disconnect successfull, return integer <0 if error occured
      * pxd_mesgErrorCode(int code) will return description of the error occured
     */
     disconnectStatusCode = pxd_PIXCIclose();
-    if(disconnectStatusCode < 0){
+    if(disconnectStatusCode < NOERROR){
         asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, 
                   "%s: disconnect camera error: %s .", 
                   driverName, pxd_mesgErrorCode(disconnectStatusCode));
@@ -120,11 +122,13 @@ Pixci::~Pixci(){
 
 
     void Pixci::acquireImage(){
+        /* TODO: implement all acquisition method like trigger, ringbuffer etc */
         static const char *functionName = "acquireImage";
         int error;
+        pxbuffer_t buffer = 1L;         // Image frame buffer
         /* live capture the image into frame buffer */
-        error = pxd_goLive(UNIT, 1L);
-        if(error < 0){
+        error = pxd_goLive(UNIT, buffer);
+        if(error < NOERROR){
             asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, 
                   "live error: %s : %s", functionName, pxd_mesgErrorCode(error));
         }
@@ -139,7 +143,7 @@ Pixci::~Pixci(){
         int error;
         /* stop the live capturing */
         error = pxd_goUnLive(UNIT);
-        if(error < 0){
+        if(error < NOERROR){
             asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, 
                   "live couldn't stop: %s : %s",functionName, pxd_mesgErrorCode(error));
         }
@@ -158,11 +162,12 @@ Pixci::~Pixci(){
 
     /**
      * @brief Acquistion task for live image capturing.
-     * Event will be notified whenever a field has beencaptured by pxd_goSnapor, pxd_goLive.
+     * Event will be notified whenever a field has been captured by pxd_goSnapor, pxd_goLive.
      */
     void Pixci::acquireTask(){
+        /* TODO: need to implement in a seperate file */
         NDArray *pImage;
-        pxbuffer_t  buf = 1;
+        pxbuffer_t  buf = 1L;
         pImage = this->pArrays[0];
         NDDataType_t  dataType;
         epicsInt32 sizeX, sizeY;
@@ -174,35 +179,39 @@ Pixci::~Pixci(){
         for (;;){
             /* waiting for event to be triggered */
             /* TODO: seperate waiting task for linux */
-            WaitForSingleObject(hEvent, INFINITE);
+            WaitForSingleObject(g_hEvent, INFINITE);
             lock();
-            getIntegerParam(NDArrayCounter, &imageCounter);
-            imageCounter++;
-            setIntegerParam(NDArrayCounter, imageCounter);;
-            getIntegerParam(ADNumImagesCounter, &numImagesCounter);
-            numImagesCounter++;
-            setIntegerParam(ADNumImagesCounter, numImagesCounter);
-            callParamCallbacks();
+
             /* Allocate NDArray */
-            sizeX = pxd_imageXdim();
-            sizeY = pxd_imageYdim();
-            dims[0] = sizeX;
-            dims[1] = sizeY;
+            dims[0] = pxd_imageXdim();
+            dims[1] = pxd_imageYdim();
             dataType = NDUInt16;
             pImage = this->pNDArrayPool->alloc(2, dims, dataType, 0, NULL);
             /* Pixel values from an image frame buffer and area of interest are copied into buffer 
             pxd_readushort(unit, framebuf, ulxc, ulyc, lrx, lry, membuf, cnt, colorspace)*/
-            pxd_readushort(UNIT, buf, 0, 0, -1, -1, (epicsUInt16*)pImage->pData, sizeX * sizeY * sizeof(epicsUInt16), "GRAY");
+            pxd_readushort(UNIT, buf, 0, 0, -1, -1, (epicsUInt16*)pImage->pData, dims[0] * dims[1] * sizeof(epicsUInt16), "GRAY");
+            
+             /* uniqueId and timeStamp must be implemented for standard ADDriver. */
             pImage->uniqueId = imageCounter;
             epicsTimeGetCurrent(&currentTime);
             pImage->timeStamp = currentTime.secPastEpoch + currentTime.nsec / 1.e9;
             updateTimeStamp(&pImage->epicsTS);
+
+            getIntegerParam(NDArrayCounter, &imageCounter);
+            getIntegerParam(ADNumImagesCounter, &numImagesCounter);
+            imageCounter++;
+            numImagesCounter++;
+            setIntegerParam(NDArrayCounter, imageCounter);
+            setIntegerParam(ADNumImagesCounter, numImagesCounter);
+            unlock();
+
+            /*Call doCallbacksGenericPointer() so that registered clients can get the values of the new arrays. 
+            Drivers must release their mutex by calling this->unlock() before they call doCallbacksGenericPointer(),
+             or a deadlock can occur if the plugin makes a call to one of the driver functions.*/
             doCallbacksGenericPointer(pImage, NDArrayData, 0);
             if (this->pArrays[0]) this->pArrays[0]->release();
             this->pArrays[0] = pImage;
             callParamCallbacks();
-            unlock();
-
         }
     }
 
