35aed690c31bcaf48caf77f8797a0f817c702c72
Subindev D <64056430+subindev-d@users.noreply.github.com>
2020-09-03 07:45:11 +0100

    removed serial task

--- pixciApp/src/pixci.h
+++ pixciApp/src/pixci.h
@@ -62,7 +62,7 @@ private:
      * @param serialOut serial command to be send to the camera
      * @return asynStatus 
      */
-    asynStatus writeSerial(int unit, char* serialOut);
+    asynStatus writeSerial(int unit, unsigned char* serialOut, int msgSize);
 
     /**
      * @brief write value to the registers of the camera using serial command
@@ -74,8 +74,7 @@ private:
      */
     asynStatus writeSerialRegister(int unit, char Register, char val);
 
-    asynStatus readSerialRegister(int unit);
+    asynStatus readSerialRegister(int unit, int value);
     void setBin(int val);
-    
-
+    int getBin();
 };
