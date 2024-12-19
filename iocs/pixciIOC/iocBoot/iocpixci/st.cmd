1d5633a21dea5d6a6cd00b6636d121ed09730da9
IrieR <175622981+irie-stfc@users.noreply.github.com>
2024-12-19 14:24:37 +0000

    Revert "remove format file from config"
    This reverts commit cc6e14359854ac786ab5d34c1cfe5ed03e050520.

--- iocs/pixciIOC/iocBoot/iocpixci/st.cmd
+++ iocs/pixciIOC/iocBoot/iocpixci/st.cmd
@@ -23,6 +23,8 @@ epicsEnvSet("NCHANS", "512")
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 epicsEnvSet("CAMERA_MODEL","4710")
+epicsEnvSet("RAPTOR_SETTINGS_FILE","$(ADPIXCI)/formatFiles/Raptor_Photonics_EagleXV_$(CAMERA_MODEL).fmt")
+
 
 #pixciConfig(portName, maxBuffers,maxMemory,priority,stackSize,formatfile)
 pixciConfig("$(PORT)", 0,  0, 0, 0, $(CAMERA_MODEL))
