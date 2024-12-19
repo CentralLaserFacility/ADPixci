0f9fbb57819a22c2806059bb21f0f650da21a300
Irie Railton <175622981+irie-stfc@users.noreply.github.com>
2024-12-19 16:51:41 +0000

    fix config call in example ioc

--- iocs/pixciIOC/iocBoot/iocpixci/st.cmd
+++ iocs/pixciIOC/iocBoot/iocpixci/st.cmd
@@ -27,7 +27,7 @@ epicsEnvSet("RAPTOR_SETTINGS_FILE","$(ADPIXCI)/videoSettings/Raptor_Eagle_XV_$(C
 
 
 #pixciConfig(portName, maxBuffers,maxMemory,priority,stackSize,formatfile)
-pixciConfig("$(PORT)", 0,  0, 0, 0, $(CAMERA_MODEL))
+pixciConfig("$(PORT)", 0,  0, 0, 0, $(CAMERA_MODEL), $(RAPTOR_SETTINGS_FILE))
 dbLoadRecords("$(ADPIXCI)/db/Pixci.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 NDStdArraysConfigure("Image1", 20, 0, "$(PORT)", 0, 0, 0, 0, 0, 5)
