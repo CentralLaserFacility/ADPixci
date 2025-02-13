9a2b4e507217843d12f88979a7dccb19504a6640
IrieR <175622981+irie-stfc@users.noreply.github.com>
2025-02-13 12:51:41 +0000

    revert last change

--- pixciApp/src/pixci.cpp
+++ pixciApp/src/pixci.cpp
@@ -590,8 +590,6 @@ void Pixci::paramTask()
         {
             updateADTemperatureActual();
             updateTemperaturePcb(epicsTrue);
-            epicsFloat64 tecTemperature = getTecTemperature();
-            setStatIfHigher(status, setDoubleParam(ADTemperature, tecTemperature));
         }
         else if (function == ADAcquirePeriod)
         {
