92c3c2ed71997d05122ceffd68d8b600646974fb
IrieR <175622981+irie-stfc@users.noreply.github.com>
2025-07-01 13:16:46 +0100

    remove lock/unlock from writeReadSerial, it breaks redback values for some reason

--- pixciApp/src/ADPixci.cpp
+++ pixciApp/src/ADPixci.cpp
@@ -968,7 +968,6 @@ epicsInt32 ADPixci::writeReadSerial(epicsInt32 unit, char *serialOut, epicsInt32
     epicsInt32 inMsgwait = 0;
     epicsInt32 inMsgwaitFlag = 0;
 
-    this->unlock();
     // checking if any message packer left to read, and clear the buffer by reading it
     if (pxd_serialRead(unit, RESERVED, nullptr, 0) > 0)
     {
@@ -1005,7 +1004,6 @@ epicsInt32 ADPixci::writeReadSerial(epicsInt32 unit, char *serialOut, epicsInt32
     {
         asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s: Failed to serial write to camera unit. Error code: %s.",
             driverName, pxd_mesgErrorCode(count));
-        this->lock();
         return count;
     }
     else
@@ -1027,7 +1025,6 @@ epicsInt32 ADPixci::writeReadSerial(epicsInt32 unit, char *serialOut, epicsInt32
                 driverName, pxd_mesgErrorCode(count));
         }
     }
-    this->lock();
     return count;
 }
 
